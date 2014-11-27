43aceaf54b17ecca73070b94359f7a937e1eb81c
Stuart Wakefield <stuart.wakefield@bbc.co.uk>
2014-11-27 09:37:23 +0000

    geometry.createLine(start, end) and Line(start, end)

--- test/geometry_test.js
+++ test/geometry_test.js
@@ -3,10 +3,22 @@ var geometry = require('../lib/geometry');
 
 describe('geometry', function () {
 	describe('.createPoint(x, y)', function () {
-		it('Creates a point with the provided coordinates', function () {
+		it('Creates a point from the provided coordinates', function () {
 			var point = geometry.createPoint(10.0, 20.0);
 			assert(point.x() === 10.0);
 			assert(point.y() === 20.0);
+		});
+	});
+
+	describe('.createLine(start, end)', function () {
+		it('Creates a line from the provided points', function () {
+			var start = geometry.createPoint(0.0, 0.0);
+			var end = geometry.createPoint(20.0, 20.0);
+			var line = geometry.createLine(start, end);
+			assert(line.start().x() == 0.0);
+			assert(line.start().y() == 0.0);
+			assert(line.end().x() == 20.0);
+			assert(line.end().y() == 20.0);
 		});
 	});
 });
